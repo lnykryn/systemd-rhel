87eaadf84215ef31e453d26c4935f82ec7ab1ef0
Lennart Poettering <lennart@poettering.net>
2013-12-03 16:41:06 +0100

    macro: better make IN_SET() macro use const arrays
    Conflicts:
    	src/shared/macro.h
    
    (cherry-picked from 059d9fbb)
    
    Resolves: #1147524

--- src/shared/macro.h
+++ src/shared/macro.h
@@ -287,4 +287,18 @@ do {                                                                    \
 #define SET_FLAG(v, flag, b) \
         (v) = (b) ? ((v) | (flag)) : ((v) & ~(flag))
 
+#define IN_SET(x, ...)                                                  \
+        ({                                                              \
+                const typeof(x) _x = (x);                               \
+                unsigned _i;                                            \
+                bool _found = false;                                    \
+                for (_i = 0; _i < sizeof((const typeof(_x)[]) { __VA_ARGS__ })/sizeof(const typeof(_x)); _i++) \
+                        if (((const typeof(_x)[]) { __VA_ARGS__ })[_i] == _x) { \
+                                _found = true;                          \
+                                break;                                  \
+                        }                                               \
+                _found;                                                 \
+        })
+
+
 #include "log.h"
