99c06d718c91c91d0992869777c15b457f95443a
Greg KH <gregkh@linuxfoundation.org>
2014-03-14 04:43:04 +0000

    machine-id: add --root option to operate on an alternate fs tree
    This makes it possible to initialize the /etc/machine-id file on an
    arbitrary filesystem hierarchy.  This helps systems that wish to run
    this at image creation time in a subdirectory, or from initramfs before
    pivot-root is called.
    
    [tomegun: converted to using _cleanup_free_ macros]
    
    Conflicts:
    	man/systemd-machine-id-setup.xml
    	src/machine-id-setup/machine-id-setup-main.c
    
    (cherry picked from commit 92f2f92edcad46ce4098ee26504edca0a1dad68e)
    
    Related: #1111199

--- src/core/main.c
+++ src/core/main.c
@@ -1503,7 +1503,7 @@ int main(int argc, char *argv[]) {
                 kmod_setup();
 #endif
                 hostname_setup();
-                machine_id_setup();
+                machine_id_setup("");
                 loopback_setup();
 
                 test_mtab();
