9a50a5c59972d3d9c244fc78efa7c400ed9dcc7c
Lennart Poettering <lennart@poettering.net>
2015-11-13 17:13:55 +0100

    core: add new DefaultTasksMax= setting for system.conf
    This allows initializing the TasksMax= setting of all units by default
    to some fixed value, instead of leaving it at infinity as before.
    
    Cherry-picked from: 0af20ea2ee2af2bcf2258e7a8e1a13181a6a75d6
    Related: #1337244

--- src/core/dbus-manager.c
+++ src/core/dbus-manager.c
@@ -2039,6 +2039,7 @@ const sd_bus_vtable bus_manager_vtable[] = {
         SD_BUS_WRITABLE_PROPERTY("ShutdownWatchdogUSec", "t", bus_property_get_usec, bus_property_set_usec, offsetof(Manager, shutdown_watchdog), 0),
         SD_BUS_PROPERTY("ControlGroup", "s", NULL, offsetof(Manager, cgroup_root), 0),
         SD_BUS_PROPERTY("SystemState", "s", property_get_system_state, 0, 0),
+        SD_BUS_PROPERTY("DefaultTasksMax", "t", NULL, offsetof(Manager, default_tasks_max), SD_BUS_VTABLE_PROPERTY_CONST),
 
         SD_BUS_METHOD("GetUnit", "s", "o", method_get_unit, SD_BUS_VTABLE_UNPRIVILEGED),
         SD_BUS_METHOD("GetUnitByPID", "u", "o", method_get_unit_by_pid, SD_BUS_VTABLE_UNPRIVILEGED),
