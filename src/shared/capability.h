38d9fd010f6f625734d1b8ccf82139f81a7bfb6e
Ismo Puustinen <ismo.puustinen@intel.com>
2015-12-31 14:54:44 +0200

    capabilities: added support for ambient capabilities.
    This patch adds support for ambient capabilities in service files. The
    idea with ambient capabilities is that the execed processes can run with
    non-root user and get some inherited capabilities, without having any
    need to add the capabilities to the executable file.
    
    You need at least Linux 4.3 to use ambient capabilities. SecureBit
    keep-caps is automatically added when you use ambient capabilities and
    wish to change the user.
    
    An example system service file might look like this:
    
    [Unit]
    Description=Service for testing caps
    
    [Service]
    ExecStart=/usr/bin/sleep 10000
    User=nobody
    AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW
    
    After starting the service it has these capabilities:
    
    CapInh: 0000000000003000
    CapPrm: 0000000000003000
    CapEff: 0000000000003000
    CapBnd: 0000003fffffffff
    CapAmb: 0000000000003000
    
    Cherry-picked from: 755d4b6
    Resolves: #1165316

--- src/shared/capability.h
+++ src/shared/capability.h
@@ -36,6 +36,9 @@ int capability_bounding_set_drop_usermode(uint64_t keep);
 
 int drop_privileges(uid_t uid, gid_t gid, uint64_t keep_capabilites);
 
+int capability_ambient_set_apply(uint64_t set, bool also_inherit);
+int capability_update_inherited_set(cap_t caps, uint64_t ambient_set);
+
 int drop_capability(cap_value_t cv);
 
 DEFINE_TRIVIAL_CLEANUP_FUNC(cap_t, cap_free);
