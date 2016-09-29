44d78fab5f11136254d4ba0c73ecb1dfc2f39293
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-09-29 16:06:02 +0200

    pid1: process zero-length notification messages again
    This undoes 531ac2b234. I acked that patch without looking at the code
    carefully enough. There are two problems:
    - we want to process the fds anyway
    - in principle empty notification messages are valid, and we should
      process them as usual, including logging using log_unit_debug().
    
    Cherry-picked from: 8523bf7dd514a3a2c6114b7b8fb8f308b4f09fc4
    Resolves: #1382284

--- src/core/manager.c
+++ src/core/manager.c
@@ -1610,13 +1610,12 @@ static int manager_dispatch_cgroups_agent_fd(sd_event_source *source, int fd, ui
         return 0;
 }
 
-static void manager_invoke_notify_message(Manager *m, Unit *u, pid_t pid, char *buf, size_t n, FDSet *fds) {
+static void manager_invoke_notify_message(Manager *m, Unit *u, pid_t pid, const char *buf, FDSet *fds) {
         _cleanup_strv_free_ char **tags = NULL;
 
         assert(m);
         assert(u);
         assert(buf);
-        assert(n > 0);
 
         tags = strv_split(buf, "\n\r");
         if (!tags) {
@@ -1683,11 +1682,6 @@ static int manager_dispatch_notify_fd(sd_event_source *source, int fd, uint32_t 
                         continue;
                 }
 
-                if (n == 0) {
-                        log_debug("Got zero-length notification message. Ignoring.");
-                        continue;
-                }
-
                 for (cmsg = CMSG_FIRSTHDR(&msghdr); cmsg; cmsg = CMSG_NXTHDR(&msghdr, cmsg)) {
                         if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS) {
 
@@ -1729,19 +1723,19 @@ static int manager_dispatch_notify_fd(sd_event_source *source, int fd, uint32_t 
                  * to avoid notifying the same one multiple times. */
                 u1 = manager_get_unit_by_pid(m, ucred->pid);
                 if (u1) {
-                        manager_invoke_notify_message(m, u1, ucred->pid, buf, n, fds);
+                        manager_invoke_notify_message(m, u1, ucred->pid, buf, fds);
                         found = true;
                 }
 
                 u2 = hashmap_get(m->watch_pids1, LONG_TO_PTR(ucred->pid));
                 if (u2 && u2 != u1) {
-                        manager_invoke_notify_message(m, u2, ucred->pid, buf, n, fds);
+                        manager_invoke_notify_message(m, u2, ucred->pid, buf, fds);
                         found = true;
                 }
 
                 u3 = hashmap_get(m->watch_pids2, LONG_TO_PTR(ucred->pid));
                 if (u3 && u3 != u2 && u3 != u1) {
-                        manager_invoke_notify_message(m, u3, ucred->pid, buf, n, fds);
+                        manager_invoke_notify_message(m, u3, ucred->pid, buf, fds);
                         found = true;
                 }
 
