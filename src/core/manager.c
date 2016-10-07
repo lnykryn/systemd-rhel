72d8c92eb1d1fec8981bb4fb8b92fde5f79487cd
Lukas Nykryn <lnykryn@redhat.com>
2016-10-07 13:24:58 +0200

    pid1: don't return any error in manager_dispatch_notify_fd() (#4240)
    If manager_dispatch_notify_fd() fails and returns an error then the
    handling of
    service notifications will be disabled entirely leading to a compromised
    system.
    
    For example pid1 won't be able to receive the WATCHDOG messages anymore
    and
    will kill all services supposed to send such messages.
    Inspired by: 9987750e7a4c62e0eb8473603150596ba7c3a015
    Resolves: #1382284

--- src/core/manager.c
+++ src/core/manager.c
@@ -1670,15 +1670,22 @@ static int manager_dispatch_notify_fd(sd_event_source *source, int fd, uint32_t 
 
                 n = recvmsg(m->notify_fd, &msghdr, MSG_DONTWAIT|MSG_CMSG_CLOEXEC);
                 if (n < 0) {
-                        if (errno == EAGAIN || errno == EINTR)
+                        if (errno == EAGAIN || errno == EINTR) {
+                                log_error("Failed to receive notification message: %m");
                                 break;
+                        }
 
-                        return -errno;
+                        /* It's not an option to return an error here since it
+                        * would disable the notification handler entirely. Services
+                        * wouldn't be able to send the WATCHDOG message for
+                        * example... */
+
+                        continue;
                 }
 
                 if (n == 0) {
                         log_debug("Got zero-length notification message. Ignoring.");
-                        return 0;
+                        continue;
                 }
 
                 for (cmsg = CMSG_FIRSTHDR(&msghdr); cmsg; cmsg = CMSG_NXTHDR(&msghdr, cmsg)) {
@@ -1701,7 +1708,8 @@ static int manager_dispatch_notify_fd(sd_event_source *source, int fd, uint32_t 
                         r = fdset_new_array(&fds, fd_array, n_fds);
                         if (r < 0) {
                                 close_many(fd_array, n_fds);
-                                return log_oom();
+                                log_oom();
+                                continue;
                         }
                 }
 
