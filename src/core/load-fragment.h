4365589534812dc2dc4d015f3459c82a440ec44f
Mike Gilbert <floppymaster@gmail.com>
2017-01-10 02:39:05 -0500

    build-sys: add check for gperf lookup function signature (#5055)
    gperf-3.1 generates lookup functions that take a size_t length
    parameter instead of unsigned int. Test for this at configure time.
    
    Fixes: https://github.com/systemd/systemd/issues/5039
    (cherry picked from commit c9f7b4d356a453a01aa77a6bb74ca7ef49732c08)

--- src/core/load-fragment.h
+++ src/core/load-fragment.h
@@ -112,7 +112,7 @@ int config_parse_protect_system(const char* unit, const char *filename, unsigned
 int config_parse_bus_name(const char* unit, const char *filename, unsigned line, const char *section, unsigned section_line, const char *lvalue, int ltype, const char *rvalue, void *data, void *userdata);
 
 /* gperf prototypes */
-const struct ConfigPerfItem* load_fragment_gperf_lookup(const char *key, unsigned length);
+const struct ConfigPerfItem* load_fragment_gperf_lookup(const char *key, GPERF_LEN_TYPE length);
 extern const char load_fragment_gperf_nulstr[];
 
 typedef enum Disabled {
--- src/shared/arphrd-list.c
+++ src/shared/arphrd-list.c
@@ -26,7 +26,7 @@
 #include "util.h"
 #include "arphrd-list.h"
 
-static const struct arphrd_name* lookup_arphrd(register const char *str, register unsigned int len);
+static const struct arphrd_name* lookup_arphrd(register const char *str, register GPERF_LEN_TYPE len);
 
 #include "arphrd-to-name.h"
 #include "arphrd-from-name.h"
