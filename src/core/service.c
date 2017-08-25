23e51f4711d3152f8c318bbeb082ffe1cd163119
Michal Sekletar <msekletar@users.noreply.github.com>
2017-08-25 15:36:10 +0200

    service: attempt to execute next main command only for oneshot services (#6619)
    This commit fixes crash described in
    https://github.com/systemd/systemd/issues/6533
    
    Multiple ExecStart lines are allowed only for oneshot services
    anyway so it doesn't make sense to call service_run_next_main() with
    services of type other than SERVICE_ONESHOT.
    
    Referring back to reproducer from the issue, previously we didn't observe
    this problem because s->main_command was reset after daemon-reload hence
    we never reached the assert statement in service_run_next_main().
    
    Fixes #6533
    
    (cherry picked from commit b58aeb70dbd1cab5908b003ef5187da1fc241839)
    
    Related: #1404657, #1471230

--- src/core/service.c
+++ src/core/service.c
@@ -2612,6 +2612,7 @@ static void service_sigchld_event(Unit *u, pid_t pid, int code, int status) {
 
                 if (s->main_command &&
                     s->main_command->command_next &&
+                    s->type == SERVICE_ONESHOT &&
                     f == SERVICE_SUCCESS) {
 
                         /* There is another command to *
