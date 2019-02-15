systemd/systemd

shared/sleep-config: exclude zram devices from hibernation candidates
On a host with sufficiently large zram but with no actual swap, logind will
respond to CanHibernate() with yes. With this patch, it will correctly respond
no, unless there are other swap devices to consider.

src/shared/sleep-config.c
@@ -21,6 +21,7 @@
 #include "log.h"
 #include "macro.h"
 #include "parse-util.h"
+#include "path-util.h"
 #include "sleep-config.h"
 #include "string-util.h"
 #include "strv.h"
@@ -201,9 +202,18 @@ int find_hibernate_location(char **device, char **type, size_t *size, size_t *us
                         continue;
                 }
 
-                if (streq(type_field, "partition") && endswith(dev_field, "\\040(deleted)")) {
-                        log_warning("Ignoring deleted swapfile '%s'.", dev_field);
-                        continue;
+                if (streq(type_field, "partition")) {
+                        if (endswith(dev_field, "\\040(deleted)")) {
+                                log_warning("Ignoring deleted swapfile '%s'.", dev_field);
+                                continue;
+                        }
+
+                        const char *fn;
+                        fn = path_startswith(dev_field, "/dev/");
+                        if (fn && startswith(fn, "zram")) {
+                                log_debug("Ignoring compressed ram swap device '%s'.", dev_field);
+                                continue;
+                        }
                 }
                 if (device)
                         *device = TAKE_PTR(dev_field);

lnykryn/systemd-rhel

(cherry picked from commit 411ae92b407bd7b4549b205ad754bcd0e3dfd81f)
Resolves: #1609816

src/shared/sleep-config.c
@@ -25,6 +25,7 @@
 #include "sleep-config.h"
 #include "fileio.h"
 #include "log.h"
+#include "path-util.h"
 #include "strv.h"
 #include "util.h"
 
@@ -202,9 +203,19 @@ static int hibernation_partition_size(size_t *size, size_t *used) {
                         continue;
                 }
 
-                if (streq(type, "partition") && endswith(dev, "\\040(deleted)")) {
-                        log_warning("Ignoring deleted swapfile '%s'.", dev);
-                        continue;
+                if (streq(type, "partition")) {
+                        const char *fn;
+
+                        if (endswith(dev, "\\040(deleted)")) {
+                                log_warning("Ignoring deleted swapfile '%s'.", dev);
+                                continue;
+                        }
+
+                        fn = path_startswith(dev, "/dev/");
+                        if (fn && startswith(fn, "zram")) {
+                                log_debug("Ignoring compressed ram swap device '%s'.", dev);
+                                continue;
+                        }
                 }
 
                 *size = size_field;